6a30e79dfcd381a18b0823a4b61f3f31faf3832a
oleg-serf <olegserfcompany@gmail.com>
2020-08-14 17:06:06 +0200

    fixed header with shadow

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Link from 'gatsby-link'
-import PoimistoLogo from './PoimistoLogo';
+import PoimistoLogo from './PoimistoLogo'
 
 import * as S from './styled'
 
 const Header = () => {
+  const [shadow, setShadow] = useState<boolean>(
+    !!globalThis.window && !!globalThis.window.scrollY
+  )
   const data = useStaticQuery<import('generated/graphql').NavigationQuery>(
     query
   )
+  // TODO: Add throttle/debounce
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 0) {
+        setShadow(true)
+      } else {
+        setShadow(false)
+      }
+      console.log(window.scrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [setShadow])
 
   const { mainNavigation } = data.site.siteMetadata
 
   return (
-    <S.HeaderWrapper>
+    <S.HeaderWrapper shadow={shadow}>
       <S.Header>
-   
         <Link to="/">
           <PoimistoLogo width={120} height={80} />
         </Link>
-  
+
         <S.Nav>
           {mainNavigation.map(navItem => (
             <S.NavItem key={navItem.id}>
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,23 +1,37 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { MdMenu } from 'react-icons/md'
 
 import { mediaQueries } from '@/assets/themes/media'
+import { container } from '@/assets/themes/helpers'
 
-export const HeaderWrapper = styled.div`
+type HeaderWrapperProps = {
+  shadow: boolean
+}
+
+export const HeaderWrapper = styled.div<HeaderWrapperProps>`
+  position: fixed;
+  background-color: #fff;
+  ${props =>
+    props.shadow
+      ? css`
+          box-shadow: 0px 1px 50px 20px rgba(100, 100, 100, 0.15);
+        `
+      : ''};
   width: 100%;
   height: 80px;
   top: 0;
   left: 0;
+  z-index: 999;
 `
 
 export const Header = styled.header`
+  ${container}
   display: flex;
   align-items: center;
   justify-content: space-between;
 `
 
 export const LogoWrapper = styled.div``
-
 
 export const Menu = styled(MdMenu).attrs(() => ({
   size: 36,
